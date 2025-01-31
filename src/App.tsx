2e24b2227009b464483bda56103701cdecaf9405
nwulu uchechukwu prince <uchenwulu@zuvy.co>
2025-01-31 11:08:48 +0100

    update state

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,31 @@ import { useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import Result from "./components/Result";
+import { ResultData } from "./components/dataType";
 
 function App() {
-  // const [results, setResult] = useState();
+  const [results, setResult] = useState<ResultData[]>([]);
+  const categoryOptions = [
+    "all categories",
+    "groceries",
+    "utitlities",
+    "rent",
+    "mortgage",
+    "insurance",
+    "transportation",
+    "health",
+    "entertainment",
+    "clothing",
+    "other",
+  ];
   return (
     <div className="app-box">
-      <Form />
+      <Form
+        categoryOptions={categoryOptions}
+        onClickSubmit={(res) => setResult([...results, res])}
+      />
       <div className="mt-5">
-      {/* <Result /> */}
+        <Result results={results} categoryOptions={categoryOptions} />
       </div>
     </div>
   );
